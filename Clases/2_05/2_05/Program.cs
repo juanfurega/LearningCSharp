0b8e57ffc888eeb6ee5b6dc1d6f5d3e854f427db
juanfurega <juancruzfurega@outlook.es>
2024-02-16 12:10:18 -0300

    new archives and object programming

---
+++ Clases/2_05/2_05/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.CalcularAreaCuadrado(5));
+            Console.WriteLine(Program.CalcularAreaCirculo(5));
+            Console.WriteLine(Program.CalcularAreaTriangulo(5, 5));
+        }
+
+        static public double CalcularAreaCuadrado(double longitudLado) 
+        {
+            double areaTotal;
+            areaTotal = Math.Pow(longitudLado, 2);
+            return areaTotal;
+        }
+
+        static public double CalcularAreaTriangulo(double t_base, double t_altura) 
+        {
+            double areaTotal;
+            areaTotal = (t_base * t_altura) / 2;
+            return areaTotal;
+        }
+
+        static public double CalcularAreaCirculo(double radio) 
+        {
+            double areaTotal;
+            areaTotal = double.Pi * Math.Pow(radio, 2);
+            return areaTotal;
+        }
+    }
+}
---
+++ Object/3_01/3_01/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection.Emit;
+
+namespace SistemaBancario
+{
+  public class Program
+    {
+        static void Main(string[] args) 
+        {
+            Cuenta usuario1 = new Cuenta();
+
+            //  parametros iniciales
+            usuario1.titular = "Juan";
+            usuario1.cantidad = 10000;
+
+            //  muestro parametros iniciales
+            string infoUsuario = usuario1.getTitular();
+            Console.WriteLine(infoUsuario);
+
+            float saldoUsuario = usuario1.getCantidad();
+            Console.WriteLine(saldoUsuario);
+
+            //  modifico
+            usuario1.Ingresar(10000);
+            usuario1.Retirar(30000);
+            
+            //  output esperado: -10000
+            usuario1.Mostrar();
+        }
+    }
+    public class Cuenta
+    {
+        public string titular;
+        public float cantidad;
+
+        public Cuenta() //  constructor
+        {
+            titular = "";
+            cantidad = 0;
+        }
+
+        public string getTitular()  //  getter titular
+        {
+            return titular;
+        }
+
+        public float getCantidad()  //  getter cantidad
+        {
+            return cantidad;
+        }
+
+        public void Mostrar()   //  muestro datos
+        {
+            Console.WriteLine("Usuario: {0}\nDinero: {1}", titular, cantidad);
+        }
+
+        public void Ingresar(float monto)   //  ingreso
+        {
+            if(monto > 0) 
+            {
+                cantidad += monto;
+            }
+        }
+
+        public void Retirar(float monto)    //  retiro
+        {
+            cantidad -= monto;
+        }
+    }
+}
